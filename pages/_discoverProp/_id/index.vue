c211d58ac9605da30d14d0e3d10b5ac431874fb0
Eden Baui <eedenbaui@gmail.com>
2021-06-17 18:17:20 +0800

    page for each movie added

--- pages/_discoverProp/_id/index.vue
+++ pages/_discoverProp/_id/index.vue
@@ -1,16 +1,113 @@
 <template>
-  <div>{{ $route.params.id }}</div>
+  <div class="container mt-5">
+    <div class="row d-flex align-items-start">
+      <div class="col-8">
+        <h2>{{ movieDetails.title }}</h2>
+        <span class="text-center text-muted"
+          >{{ movieDetails.runtime }} min | {{ movieDetails.releaseDate }}</span
+        >
+        <div class="ratio ratio-16x9 mt-3">
+          <h3></h3>
+          <iframe
+            width="560"
+            height="315"
+            :src="'https://www.youtube.com/embed/' + youtubeKey"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+      <div class="col-4">
+        <h5>Overview</h5>
+        <p>
+          {{ movieDetails.overview }}
+        </p>
+
+        <div class="d-flex align-items-center mb-3">
+          <h5 class="d-inline me-2 mb-0">Genres</h5>
+          <span
+            class="badge rounded-pill bg-secondary me-2"
+            v-for="genre in movieDetails.genres"
+            :key="genre.id"
+          >
+            {{ genre.name }}
+          </span>
+        </div>
+
+        <a
+          :href="movieDetails.website"
+          target="_blank"
+          class="
+            btn btn-warning
+            d-flex
+            align-items-center
+            justify-content-center
+            rounded-3
+          "
+          ><span class="material-icons"> open_in_new </span> Website</a
+        >
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      baseURL: "https://api.themoviedb.org/3/movie",
+      apiKey: process.env.API_KEY,
+      youtubeKey: null,
+      movieDetails: {},
+    };
   },
-  methods: {},
-  mounted() {},
+  methods: {
+    async fetchTrailer() {
+      try {
+        const url = `${this.baseURL}/${this.$route.params.id}/videos?api_key=${this.apiKey}&language=en-US`;
+        const response = await fetch(url);
+        const data = await response.json();
+        this.youtubeKey = data.results[0].key;
+      } catch (error) {
+        console.log("an error occured while fetching trailer ", error);
+      }
+    },
+    async fetchmovieDetails() {
+      try {
+        const url = `${this.baseURL}/${this.$route.params.id}?api_key=${this.apiKey}&language=en-US`;
+        const response = await fetch(url);
+        const data = await response.json();
+        this.movieDetails = {
+          genres: data.genres,
+          id: data.id,
+          runtime: data.runtime,
+          imdb: data.imdb_id,
+          title: data.title,
+          overview: data.overview,
+          website: data.homepage,
+          releaseDate: new Date(data.release_date)
+            .toDateString()
+            .split(" ")
+            .slice(1)
+            .join(" "),
+        };
+        console.log("genre", this.movieDetails);
+      } catch (error) {
+        console.log("an error occured while fetching movie details ", error);
+      }
+    },
+  },
+  mounted() {
+    this.fetchTrailer();
+    this.fetchmovieDetails();
+  },
 };
 </script>
 
 <style>
+.video-size {
+  width: 800px;
+}
 </style>
