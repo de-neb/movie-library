24e867113a5d2a3e5d385b6efa70590c20b0f7ef
Eden Baui <eedenbaui@gmail.com>
2021-09-09 14:56:39 +0800

    updated configs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
+  router: {
+    base: "/movie-library/"
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
